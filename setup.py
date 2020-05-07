b33f66913d02feaa4e7491676843595d08ac231c
Max Eisner <4730112+max-OS1@users.noreply.github.com>
2020-05-07 22:15:30 +0200

    initial setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='rbg2player',
+    version='0.1',
+    py_modules=['rbg2player'],
+    install_requires=['click', 'validators', 'requests'],
+    entry_points='''
+        [console_scripts]
+        rbg2player=rbg2player:main
+    ''',
+    python_requires='>=3',
+)
